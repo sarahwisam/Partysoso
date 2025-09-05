6ce80e62059fe556e38162d5b9c77df819794bdb
sarahwisam <sarahwisam42@gmail.com>
2025-09-05 16:38:00 +0300

    upadate in children

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,7 +11,11 @@ export const metadata = {
   description: 'Full starter with App Router and Tailwind CSS.',
 }
 
-export default function RootLayout({ children }) {
+export default function RootLayout({ children,
+}: {
+  children: React.ReactNode ;
+
+}) {
   return (
     <html lang="en" className={'${playfair.variable}'}>
       <body className="min-h-screen flex flex-col">{children}</body>
